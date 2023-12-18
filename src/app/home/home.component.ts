8a8d419940efc5e3eac404ef598b363072dd0687
remystreich <remystreich@gmail.com>
2023-12-18 16:04:22 +0100

    routage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
-import { HousingLocation } from '../housinglocation';
+import { HousingLocation } from '../housingLocation';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-home',
@@ -16,21 +17,23 @@ import { HousingLocation } from '../housinglocation';
     </section>
 
     <section class="results">
-      <app-housing-location></app-housing-location>
+      @for (housingLocation of housingLocationList; track housingLocation.name ) {
+
+        <app-housing-location
+        [housingLocation]="housingLocation"
+        ></app-housing-location>
+      }
+    
     </section>
   `,
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  readonly baseUrl = 'https://angular.dev/assets/tutorials/common';
-  housingLocation: HousingLocation = {
-    id: 9999,
-    name: 'Test Home',
-    city: 'Test city',
-    state: 'ST',
-    photo: `${this.baseUrl}/example-house.jpg`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  };
-}
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
+};
+
